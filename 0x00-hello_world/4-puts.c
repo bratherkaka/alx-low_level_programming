b906f611fb9f017245e6e4748a9039499c71203d
Your Name <you@example.com>
2022-09-08 15:48:33 -0700

    Joseph first C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main -F Printing
-*
-*Retun: Always 0 (Successful)
-*/
+ * main - Fprinting
+ *
+ * Return: Always 0 (Successful)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building aa multilingual puzzle");
+	return (0);
 }
