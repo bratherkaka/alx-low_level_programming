1603e2c18db2389bca2ed74bbd2c748fb8760aea
Your Name <you@example.com>
2022-09-08 15:19:01 -0700

    Joseph first C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-
-* main - print the string in the put function
-
+*main -Printing
 *
-
-* Description: using the main function
-
-* this program prints "Programming is like building a multilingual puzzle
-
-* Return: 0
-
+*return : Alyways 0
 */
-
 int main(void)
-
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
-
+puts("\"Programing is like builing a multilingual");
+return (0);
 }
