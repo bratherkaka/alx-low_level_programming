5dd7bdea48955cd46c0f9d8c3642040048fed577
Your Name <you@example.com>
2022-09-12 14:07:58 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,6 @@
  *
  *
  *Description: using the main function
- *
  *this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
@@ -18,15 +17,13 @@ int main(void)
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-
-			putchar(ch);
+	putchar(ch);
 
 	}
 
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
 	{
-
-			putchar(ch);
+	putchar(ch);
 
 	}
 
