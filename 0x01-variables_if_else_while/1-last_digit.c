cac17dc7ca2190dc6a38488296b71510f964337a
Your Name <you@example.com>
2022-09-12 12:53:12 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,15 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /**
-
  * main - Prints the last digit of a randomly generated number
-
- * and whether it is greater than 5, less than 6, or 0.
-
  *
-
+ * and whether it is greater than 5, less than 6, or 0.
+ *
  * Return: Always 0.
-
  */
 
 int main(void)
@@ -31,9 +27,7 @@ int main(void)
 
 	{
 
-		printf("Last digit of %d is %d and is greater than 5\n",
-
-			n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 
 	}
 
@@ -51,9 +45,7 @@ int main(void)
 
 	{
 
-		printf("Last digit of %d is %d and is 0\n",
-
-			n, n % 10);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 
 	}
 
