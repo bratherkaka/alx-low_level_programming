647766ba3266d4775a75d635ad18cd438045ed72
Your Name <you@example.com>
2022-09-16 15:41:12 -0700

    js

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- *main- prints the largest prime factor
- *of a number
- *Return: returns 0
+ * main- prints the largest prime factor
+ * of a number
+ * Return: returns 0
  */
-
 int main(void)
 {
 	long number = 612852475143;
