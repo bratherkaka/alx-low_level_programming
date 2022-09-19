c9fa4d74b26b9a7ed210c8f7d55392ae79abb4af
Josph Simon <kakabrather@gmail.com>
2022-09-19 18:24:47 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,15 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdlib.h>
-
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+#include <stdio.h>
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
-
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 #endif
