697370d6bba36213f74fb661d3a754bdc07e8133
Your Name <you@example.com>
2022-09-30 01:19:20 -0700

    Arg

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,5 @@
-#ifndef __HOLBERTON__
-#define __HOLBERTON__
+#ifndef __MAIN__
+#define __MAIN__
 int _putchar(char c);
 int is_integer(const char *str);
 int get_coin_number(int amount, int nb_coins);
