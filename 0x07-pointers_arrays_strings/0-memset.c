4d6a7fef73b838a6e6ce7e9e664eefce960c8625
Josph Simon <kakabrather@gmail.com>
2023-03-15 11:43:23 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,13 +7,14 @@
  * Return: A pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
+    
 {
     unsigned int i;
-
+    
     for (i = 0; i < n; i++)
     {
         s[i] = b;
     }
-
+    
     return (s);
 }
